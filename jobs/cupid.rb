9b2eed3ddc6aba94be764da5e4ebd98f16e58979
Amaresh Ray <amareshray@gmail.com>
2013-03-27 23:29:42 +0000

    Cupid.com widget

---
+++ jobs/cupid.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+require 'net/http'
+require 'json'
+
+
+SCHEDULER.every '1m', :first_in => 0 do |job|
+  http = Net::HTTP.new("affiliates.cupid.com")
+  response = http.request(Net::HTTP::Get.new("/stats/stats.json?api_key=AFFddwfng8krV0mn7HCl1Uufp1E1ps&start_date=2013-03-01&end_date=2013-03-31"))
+  if response.code != "200"
+    puts "cupid api error (status-code: #{response.code})\n#{response.body}"
+  else 
+    data = JSON.parse(response.body)
+    clicks = data['data'].first['clicks'].to_i
+    conversions = data['data'].first['conversions'].to_i
+    payout = data['data'].first['payout'][1...-1].sub(',','').to_i
+    erpc = data['data'].first['erpc'].to_i
+    cpl = data['data'].first['cpl'].to_i
+
+    send_event('cupid_clicks', current: clicks)
+    send_event('cupid_conversions', current: conversions)
+    send_event('cupid_payout', current: payout)
+    send_event('cupid_erpc', current: erpc)
+    send_event('cupid_cpl', current: cpl)
+  end
+end
+
